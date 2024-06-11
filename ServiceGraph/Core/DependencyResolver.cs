3422b9ed9689ab189b5feebbcacc6a34546fe29d
Ismail <nakkazulav@mail.ru>
2024-06-11 21:30:05 +0400

    master:dependencyResolve Resolve()

--- ServiceGraph/Core/DependencyResolver.cs
+++ ServiceGraph/Core/DependencyResolver.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ServiceGraph.Core;
 
@@ -6,6 +7,23 @@ public class DependencyResolver
 {
     public Dictionary<Type, List<Type>> Resolve(IEnumerable<ServiceDescriptor> serviceDescriptors)
     {
-        
+        var dependencies = new Dictionary<Type, List<Type>>();
+        foreach (ServiceDescriptor serviceDescriptor in serviceDescriptors)
+        {
+            Type serviceType = serviceDescriptor.ServiceType;
+            Type? implementationType = serviceDescriptor.ImplementationType;
+
+            if (implementationType != null)
+            {
+                ConstructorInfo? ctor = implementationType.GetConstructors().FirstOrDefault();
+                if (ctor != null)
+                {
+                    List<Type> parameterTypes = ctor.GetParameters().Select(p => p.ParameterType).ToList();
+                    dependencies[serviceType] = parameterTypes;
+                }
+            }
+        }
+
+        return dependencies;
     }
 }
