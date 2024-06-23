698aa7bd61fad0c266c5df6baad202b7c2642b36
Ismail <nakkazulav@mail.ru>
2024-06-23 12:56:33 +0400

    cycle detector

---
+++ ServiceGraph/Graph/CycleDetector.cs
@@ -0,0 +1,20 @@
+﻿using QuickGraph;
+using QuickGraph.Graphviz;
+
+namespace ServiceGraph.Graph;
+
+public class CycleDetector
+{
+    private readonly GraphvizAlgorithm<Type, Edge<Type>> _graphviz;
+
+    public CycleDetector(GraphvizAlgorithm<Type, Edge<Type>> graphviz)
+    {
+        _graphviz = graphviz;
+    }
+
+    public bool HasCycle()
+    {
+        // todo continue impl
+        throw new NotImplementedException();
+    }
+}
