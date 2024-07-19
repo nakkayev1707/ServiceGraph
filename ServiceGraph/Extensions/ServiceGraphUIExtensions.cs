85402bc6b0f8e8be0359384b4a4ab2cbd53b47cb
Ismail <nakkazulav@mail.ru>
2024-07-19 23:26:34 +0400

    refactoring: remove extension method - replace with new one

--- ServiceGraph/Extensions/ServiceGraphUIExtensions.cs
+++ ServiceGraph/Extensions/ServiceGraphUIExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using ServiceGraph.Core;
 using ServiceGraph.Visualization.Core;
 
 namespace ServiceGraph.Extensions;
@@ -6,9 +7,13 @@ namespace ServiceGraph.Extensions;
 public static class ServiceGraphUIExtensions
 {
     private const string RoutePrefix = "service-graph";
+
+    public static IApplicationBuilder UseServiceGraph(this IApplicationBuilder app, ServiceGraphOption option)
+    {
+        
+    }
     
-    public static IApplicationBuilder UseServiceGraphUI(
-        this IApplicationBuilder app)
+    public static IApplicationBuilder UseServiceGraphUI(this IApplicationBuilder app)
     {
         return app.UseMiddleware<ServiceGraphUIMiddleware>();
     }
